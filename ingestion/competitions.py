17e3b02430f5ede2aa6bdb6f295da8e935e7b893
Hamza Zaidi <hamzaz012803@gmail.com>
2026-03-31 02:11:41 +0530

    Added event-based filtering using WCA competition_index API

---
+++ ingestion/competitions.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+
+BASE_URL = "https://www.worldcubeassociation.org"
+
+def fetch_competitions_page(page=1, event_id=None):
+    url = f"{BASE_URL}/api/v0/competition_index"
+    
+    params = {
+        "include_cancelled": "false",
+        "sort": "-end_date,-start_date,name",
+        "page": page,
+        "end": datetime.today().strftime("%Y-%m-%d")
+    }
+
+    if event_id:
+        params["event_ids[]"] = event_id
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_recent_competitions(limit=10, event_id=None):
+    page = 1
+    results = []
+
+    while len(results) < limit:
+        data = fetch_competitions_page(page, event_id)
+
+        if not data:
+            break
+
+        results.extend(data)
+        page += 1
+
+    return results[:limit]
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from ingestion.competitions import get_recent_competitions
+
 def main():
-    print("BLD Scramble Engine starting...")
+    comps = get_recent_competitions(5, event_id="333bf")
+
+    for c in comps:
+        print(c["id"], "-", c["name"])
 
 if __name__ == "__main__":
     main()
